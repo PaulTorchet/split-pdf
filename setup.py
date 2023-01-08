82c04f6d206b0e906fd6be5cadedfe427a058830
Paul TORCHET <ptorchet.dev@gmail.com>
2023-01-08 20:34:58 +0100

    updates to import package

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 
-setup(name='split-pdf', version='0.0.1', packages=["split-pdf"])
+setup(name='split-pdf', version='0.0.1', packages=["split_pdf"])
--- split_pdf/__init__.py
+++ split_pdf/__init__.py
@@ -1,5 +1,5 @@
-import cli
-import controller
-import pdf
-import util
-import config
+from split_pdf import cli
+from split_pdf import controller
+from split_pdf import pdf
+from split_pdf import util
+from split_pdf import config
